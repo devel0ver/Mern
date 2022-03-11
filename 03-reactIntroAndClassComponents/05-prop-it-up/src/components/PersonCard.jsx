8bff8e755ee4b173c8cd8237f6051168900abd4d
devel0ver <shayaahmed06@gmail.com>
2022-03-11 15:16:15 -0500

    first commit

--- 03-reactIntroAndClassComponents/05-prop-it-up/src/components/PersonCard.jsx
+++ 03-reactIntroAndClassComponents/05-prop-it-up/src/components/PersonCard.jsx
@@ -1,13 +1,31 @@
 import React, {Component} from "react";
 
 class PersonCard extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            // crate a state variable for the likes so its nott just in a props variable (which we were not allowed to update the value up according to react)
+
+            age: this.props.age //initialize the value of likes to be whatever value we got from props
+        };
+
+        this.ageHandler= ()=>{
+            this.setState({age: this.state.age +1}) // inn order  to update state variables, we cant update it like we would a regular variable, we must use this.setState() to update state variables.
+        }
+    }
+
     render(){
         return(
-            <div>
-                <h1>{this.props.firstName}, {this.props.lastName}</h1>
-                <p>Age: {this.props.age}</p>
-                <p>Hair Color: {this.props.hairColor}</p>
-            </div>
+            <>
+                <div>
+                    <h1>{this.props.firstName}, {this.props.lastName}</h1>
+                    <p>Age: {this.state.age}</p>
+                    <p>Hair Color: {this.props.hairColor}</p>
+                    <button className="btn btn-secondary" onClick={this.ageHandler}>Birthday Button for {this.props.firstName} {this.props.lastName}</button>
+                </div>
+                <hr />
+            </>
         )
     }
 }
