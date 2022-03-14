8bade43f6285b5c688941835fa78564f94d55a01
devel0ver <shayaahmed06@gmail.com>
2022-03-14 01:00:23 -0400

    More forms  assignment

--- 04-reactFunctionalComponents/05-hook-form/src/components/HookForm.jsx
+++ 04-reactFunctionalComponents/05-hook-form/src/components/HookForm.jsx
@@ -22,22 +22,37 @@ const HookForm= (props)=>{
                 <label className="input-group-text" htmlFor="f_name">First Name</label>
                 <input type="text" name="" id="f_name" className="form-control" onChange={(e)=>setFirstName(e.target.value)}/>
             </div>
+            {
+                firstName.length<2 && firstName.length>0? <p className="text-danger">First name must be at least 2 characters</p> : null
+            }
             <div className="input-group mt-4">
                 <label className="input-group-text" htmlFor="l_name">Last Name</label>
                 <input type="text" name="" id="l_name" className="form-control" onChange={(e)=>setLastName(e.target.value)}/>
             </div>
+            {
+                lastName.length<2 && lastName.length>0? <p className="text-danger">Last name must be at least 2 characters</p> : null
+            }
             <div className="input-group mt-4">
                 <label className="input-group-text" htmlFor="email">Email</label>
                 <input type="email" name="" id="email" className="form-control" onChange={(e)=>setEmail(e.target.value)}/>
             </div>
+            {
+                email.length<2 && email.length>0? <p className="text-danger">Email must be at least 2 characters</p> : null
+            }
             <div className="input-group mt-4">
                 <label className="input-group-text" htmlFor="password">Password</label>
                 <input type="password" name="" id="password" className="form-control" onChange={(e)=>setPassword(e.target.value)}/>
             </div>
+            {
+                password.length<8 && password.length>0? <p className="text-danger">Password must be at least 8 characters</p> : null
+            }
             <div className="input-group mt-4">
                 <label className="input-group-text" htmlFor="password">Confirm Password</label>
                 <input type="password" name="" id="password" className="form-control" onChange={(e)=>setConfirmPassword(e.target.value)}/>
             </div>
+            {
+                confirmPassword!=password && confirmPassword.length>0? <p className="text-danger">Passwords must match!</p> : null
+            }
         </form>
         <p className="text-center mt-3">Your form data</p>
         <p>First Name: <span className="ms-2">{firstName}</span></p>
