05f70d5c190b45bad04fa590ce6f8534eecadbcf
devel0ver <shayaahmed06@gmail.com>
2022-03-23 01:54:42 -0400

    Product Manager (part 1)

--- 08-mangooseAndFullStack/productManager/server/controllers/product_manager.controller.js
+++ 08-mangooseAndFullStack/productManager/server/controllers/product_manager.controller.js
@@ -16,6 +16,14 @@ module.exports.createProduct = (req, res)=>{
         .catch(err => res.json({ message: 'Something went wrong' }))
 }
 
+module.exports.getOneProduct = (req, res) => {
+    Product.findOne({ _id: req.params.id })
+        .then(foundProduct=>{
+            res.json({ results: foundProduct })
+        })
+        .catch(err => res.json({ message: 'Something went wrong' }))
+}
+
 module.exports.updateOneProduct = (req, res) => {
     Product.findOneAndUpdate(
         { _id: req.params.id },
--- 08-mangooseAndFullStack/productManager/server/routes/product_manager.route.js
+++ 08-mangooseAndFullStack/productManager/server/routes/product_manager.route.js
@@ -1,8 +1,20 @@
 const ProductController = require('../controllers/product_manager.controller')
 
 module.exports = app =>{
+
+    //find all api endpoint
     app.get('/api/products', ProductController.findAllProducts)
+
+    //create one api endpoint
     app.post('/api/products', ProductController.createProduct)
+
+    //find one by id api endpoint
+    app.get('/api/products/:id', ProductController.getOneProduct)
+
+    //update one by id api endpoint
     app.put('/api/products/:id', ProductController.updateOneProduct)
+
+    //delete one by id api endpoint
     app.delete('/api/products/:id', ProductController.deleteOneProduct)
+
 }
