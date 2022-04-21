ed7e6bb9a4dd808784c6d8059207aee7774a424a
devel0ver <shayaahmed06@gmail.com>
2022-04-21 12:56:52 -0400

    Algo

--- 00-Algos/digitalRoot.js
+++ 00-Algos/digitalRoot.js
@@ -1,11 +1,19 @@
 function digital_root(n) {
     // ...
+    // let arr = [];
+    // let sum = n;
+    // while(sum>9){
+    //     arr = sum.toString().split("");
+    //     sum = arr.reduce((previousValue, currentValue)=> parseInt(previousValue) + parseInt(currentValue), 0 
+    //     );
+    // }
+    // return sum;
     let arr = [];
     let sum = n;
+    let reduce = (a,b) => parseInt(a) + parseInt(b)
     while(sum>9){
         arr = sum.toString().split("");
-        sum = arr.reduce((previousValue, currentValue)=> parseInt(previousValue) + parseInt(currentValue), 0 
-        );
+        sum = arr.reduce(reduce);
     }
     return sum;
 }
