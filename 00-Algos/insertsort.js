5b990ce0e5324677e7e6128445c4a56af331b3db
Shaya Ahmed <shayaahmed06@gmail.com>
2025-10-25 08:26:28 -0400

    feat: add insertion sort implementation (students by grade)

--- 00-Algos/insertsort.js
+++ 00-Algos/insertsort.js
@@ -1,6 +1,7 @@
 //https://www.hackerearth.com/practice/algorithms/sorting/insertion-sort/visualize/
 
 // function insertionSort(arr){
+//     // Assume that the value of index 0 is always sorted. Start at index 1.
 //     for(let x = 1; x < arr.length; x++){
 //         console.log(arr[x])
 //         let j = x
@@ -17,3 +18,26 @@
 
 
 // console.log(insertionSort([5,23,0,12,8,6])) //[0,5,6,8,12,23]
+
+
+// Challenge: Sort Students by Grade
+
+const students = [
+    { name: "Ali", grade: 85 },
+    { name: "Sara", grade: 95 },
+    { name: "Yusuf", grade: 70 },
+    { name: "Hana", grade: 90 }
+];
+
+var sortStudentsByGrade = (arr) => {
+    for (let i = 1; i < arr.length; i++){
+        let x = i;
+        while(x > 0 && arr[x].grade < arr[x-1].grade){
+            [arr[x], arr[x-1]] = [arr[x-1], arr[x]]
+            x--;
+        }
+    }
+    return arr
+}
+
+console.log(sortStudentsByGrade(students));
