257c35a2d4bc6b0b1296ed9e117da239f4aba0a5
Shaya Ahmed <shayaahmed06@gmail.com>
2025-11-10 09:48:49 -0500

    Add dynamic age-based greeting to PersonCard component

--- 03-reactIntroAndClassComponents/05-prop-it-up/src/App.js
+++ 03-reactIntroAndClassComponents/05-prop-it-up/src/App.js
@@ -5,7 +5,7 @@ import PersonCard from './components/PersonCard';
 function App() {
   return (
     <div className="App">
-      <PersonCard firstName = "Doe" lastName = "Jane" age = {45} hairColor = "Black"></PersonCard>
+      <PersonCard firstName = "Doe" lastName = "Jane" age = {29} hairColor = "Black"></PersonCard>
       <PersonCard firstName = "Smith" lastName = "John" age = {88} hairColor = "Brown"></PersonCard>
       <PersonCard firstName = "Fillmore" lastName = "Millard" age = {50} hairColor = "Brown"></PersonCard>
       <PersonCard firstName = "Smith" lastName = "Maria" age = {62} hairColor = "Brown"></PersonCard>
--- 03-reactIntroAndClassComponents/05-prop-it-up/src/components/PersonCard.jsx
+++ 03-reactIntroAndClassComponents/05-prop-it-up/src/components/PersonCard.jsx
@@ -6,13 +6,20 @@ class PersonCard extends Component{
         super(props);
         this.state = {
             // crate a state variable for the likes so its nott just in a props variable (which we were not allowed to update the value up according to react)
-
             age: this.props.age //initialize the value of likes to be whatever value we got from props
         };
+    }
 
-        this.ageHandler= ()=>{
-            this.setState({age: this.state.age +1}) // inn order  to update state variables, we cant update it like we would a regular variable, we must use this.setState() to update state variables.
-        }
+    ageHandler= ()=>{
+        this.setState({age: this.state.age +1}) // inn order  to update state variables, we cant update it like we would a regular variable, we must use this.setState() to update state variables.
+    }
+
+    getMessage = () => {
+        let age = this.state.age;
+        // console.log(age)
+        if(age < 30) return "So young and full of energy!"
+        if(age > 29 && age < 61) return "At your prime!"
+        return "Full of wisdom."
     }
 
     render(){
@@ -22,6 +29,7 @@ class PersonCard extends Component{
                     <h1>{this.props.firstName}, {this.props.lastName}</h1>
                     <p>Age: {this.state.age}</p>
                     <p>Hair Color: {this.props.hairColor}</p>
+                    <p>{this.getMessage()}</p>
                     <button className="btn btn-secondary" onClick={this.ageHandler}>Birthday Button for {this.props.firstName} {this.props.lastName}</button>
                 </div>
                 <hr />
